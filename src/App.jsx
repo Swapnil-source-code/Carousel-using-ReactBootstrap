e800a0aef4054c3a56dedf077e0bf6330735e506
“Swapnil <swapnilkumbhar366@gmail.com>
2024-10-13 11:29:01 +0530

    Created Carousel using ReactBootstrap

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CarouselSlider from './components/Carousel/CarouselSlider';
+
+function App() {
+ 
+  return (
+    <>
+      <CarouselSlider/>
+    </>
+  )
+}
+
+export default App
